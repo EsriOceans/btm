72cab16e82f8b15e1d7c3903b7fc05d0edf96e21
Shaun Walbridge <shaun.walbridge@gmail.com>
2012-10-19 01:21:59 -0700

    set local path directory; explain variables present.

--- Install/toolbox/scripts/config.py
+++ Install/toolbox/scripts/config.py
@@ -1,1 +1,8 @@
+import os
+
+# current directory
+local_path = os.path.dirname(__file__)
+
+# default mode for tools. Expect them to be run from a Python toolbox,
+# not the command line by default.
 mode='toolbox'
