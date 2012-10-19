3e9468adceecb11a96d91f4342bd6a9897185ea1
Shaun Walbridge <shaun.walbridge@gmail.com>
2012-10-19 01:50:41 -0700

    add out CSV dictionary as a test file.

--- tests/config.py
+++ tests/config.py
@@ -5,3 +5,4 @@ import os
 local_path = os.path.dirname(__file__)
 
 xml_doc = os.path.abspath(os.path.join(local_path, 'data', 'fagatelebay_zone.xml'))
+csv_doc = os.path.abspath(os.path.join(local_path, 'data', 'fagatelebay_zone.csv'))
