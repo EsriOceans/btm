4d8ac841b2553c60cc3c98ecf897b4d5f88d249e
Shaun Walbridge <shaun.walbridge@gmail.com>
2012-10-19 01:52:24 -0700

    remove debug statement

--- Install/toolbox/scripts/utils.py
+++ Install/toolbox/scripts/utils.py
@@ -16,7 +16,6 @@ def addLocalPaths(paths):
     for path_part in paths:
         base_path = os.path.join(local_path, path_part)
         abs_path = os.path.abspath(base_path)
-        print "importing " + abs_path
         sys.path.insert(0, abs_path)
 
 def msg(output_msg, mtype='message'):
