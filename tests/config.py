25d816e5b9b5dae4cd97b836064c0744a1e59801
Shaun Walbridge <shaun.walbridge@gmail.com>
2013-07-08 13:15:11 -0700

    add variable for input bathy

--- tests/config.py
+++ tests/config.py
@@ -6,3 +6,4 @@ local_path = os.path.dirname(__file__)
 
 xml_doc = os.path.abspath(os.path.join(local_path, 'data', 'fagatelebay_zone.xml'))
 csv_doc = os.path.abspath(os.path.join(local_path, 'data', 'fagatelebay_zone.csv'))
+bathy_raster = os.path.abspath(os.path.join(local_path, 'data', 'bathy5m_clip.tif'))
