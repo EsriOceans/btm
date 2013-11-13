2b4d1bc08e9122a3fb515e1e9b57e3a8c6402beb
Shaun Walbridge <shaun.walbridge@gmail.com>
2013-11-13 11:47:45 -0500

    Update signing path to its new 10.2 location.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 echo "building Addin..."
 python ./makeaddin.py
 echo "signing Addin..."
-/c/apps/arcgis/Desktop10.1/bin/ESRISignAddin.exe c:\\data\\arcgis\\addins\\btm.esriaddin /c:c:\\data\\arcgis\\addins\\cert.cer
+"/c/Program Files (x86)/Common Files/ArcGIS/bin/ESRISignAddin.exe" c:\\data\\arcgis\\addins\\btm.esriaddin /c:c:\\data\\arcgis\\addins\\cert.cer
 echo "executing tests..."
 python tests/testMain.py
 echo "If tests passed, type 'y'; to install addin."
