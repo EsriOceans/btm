1d6fa0521b0fbba48ddbc231614b7074a63488c2
Shaun Walbridge <shaun.walbridge@gmail.com>
2013-11-13 12:54:18 -0500

    Remove debug messages from import.

--- tests/utils.py
+++ tests/utils.py
@@ -7,6 +7,4 @@ def addLocalPaths(paths):
     for path_part in paths:
         base_path = os.path.join(local_path, path_part)
         abs_path = os.path.abspath(base_path)
-        print "importing " + abs_path
         sys.path.insert(0, abs_path)
-
