7c200486a4b7d9d7701351691c139220afbf9aa7
Shaun Walbridge <shaun.walbridge@gmail.com>
2013-11-14 13:39:49 -0500

    change capitalization for consistency

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-echo "building Addin..."
+echo "building addin..."
 python ./makeaddin.py
-echo "signing Addin..."
+echo "signing addin..."
 "/c/Program Files (x86)/Common Files/ArcGIS/bin/ESRISignAddin.exe" c:\\data\\arcgis\\addins\\btm.esriaddin /c:c:\\data\\arcgis\\addins\\cert.cer
 echo "executing tests..."
 python tests/testMain.py
