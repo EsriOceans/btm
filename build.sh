9b30ac2979495a517d69ffa8a2a98c06fc63afd2
Shaun Walbridge <shaun.walbridge@gmail.com>
2014-02-04 04:22:58 -0500

    Make tests optional

--- build.sh
+++ build.sh
@@ -3,8 +3,16 @@ echo "building addin..."
 python ./makeaddin.py
 echo "signing addin..."
 "/c/Program Files (x86)/Common Files/ArcGIS/bin/ESRISignAddin.exe" c:\\data\\arcgis\\addins\\btm.esriaddin /c:c:\\data\\arcgis\\addins\\cert.cer
-echo "executing tests..."
-python tests/testMain.py
+echo "Would you like to run the test suite? y/n"
+read input
+
+if [ "$input" == "y" ]; then
+  echo "executing tests..."
+  python tests/testMain.py
+else
+  echo "skipping tests."
+fi
+
 echo "If tests passed, type 'y'; to install addin."
 read input
 
