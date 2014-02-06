108a269260664d41701dd328dbb183f5aea41b99
Shaun Walbridge <shaun.walbridge@gmail.com>
2014-02-06 03:31:42 -0500

    PEP8 compliance; #5

--- Install/toolbox/scripts/classify.py
+++ Install/toolbox/scripts/classify.py
@@ -4,7 +4,7 @@
 
 # Import system modules
 import os, sys, textwrap, arcpy
-from arcpy.sa import *
+from arcpy.sa import Con, Raster
 
 import utils
 import config
@@ -12,9 +12,10 @@ import config
 # Check out any necessary licenses
 arcpy.CheckOutExtension("Spatial")
 
-def runCon(lower_bounds, upper_bounds, in_grid, true_val, true_alt=None):
+def run_con(lower_bounds, upper_bounds, in_grid, true_val, true_alt=None):
     # debug message:
-    #utils.msg("runCon: lb: `%s`  ub: `%s` grid: `%s`  val: `%s`" % (lower_bounds, upper_bounds, in_grid, true_val))
+    #utils.msg("run_con: lb: `{}`  ub: `{}` grid: `{}`  val: `{}`".format(
+    #        lower_bounds, upper_bounds, in_grid, true_val))
     out_grid = ""
     # if our initial desired output value isn't set, use the backup
     if str(true_val) == '':
@@ -29,7 +30,8 @@ def runCon(lower_bounds, upper_bounds, in_grid, true_val, true_alt=None):
     # calculate our output grid
     if lower_bounds is not None:
         if upper_bounds is not None:
-            out_grid = Con((Raster(in_grid) > float(lower_bounds)) & (Raster(in_grid) < float(upper_bounds)), true_val, 0)
+            out_grid = Con((Raster(in_grid) > float(lower_bounds)) & \
+                    (Raster(in_grid) < float(upper_bounds)), true_val, 0)
         else:
             out_grid = Con(Raster(in_grid) >= float(lower_bounds), true_val, 0)
     elif upper_bounds is not None:
@@ -47,53 +49,54 @@ def main(classification_file, bpi_broad, bpi_fine, slope, bathy,
         # to the output folder.
         out_workspace = os.path.dirname(out_raster)
         # make sure workspace exists
-        utils.workspaceExists(out_workspace)
-        utils.msg("Set scratch workspace to %s..." % out_workspace)
+        utils.workspace_exists(out_workspace)
+        utils.msg("Set scratch workspace to {}...".format(out_workspace))
         arcpy.env.scratchWorkspace = out_workspace
         arcpy.env.workspace = out_workspace
 
         arcpy.env.overwriteOutput = True
         # Create the broad-scale Bathymetric Position Index (BPI) raster
-        msg_text="Generating the classified grid, based on the provided" +\
-            " classes in '{classes}'.".format(classes=classification_file)
+        msg_text = "Generating the classified grid, based on the provided" + \
+                " classes in '{classes}'.".format(classes=classification_file)
         utils.msg(msg_text)
-        
+
         # Read in the BTM Document; the class handles parsing a variety of inputs.
         btm_doc = utils.BtmDocument(classification_file)
         classes = btm_doc.classification()
-        utils.msg("Parsing %s document... found %i classes." % (btm_doc.doctype, len(classes)))
+        utils.msg("Parsing {} document... found {} classes.".format(
+            btm_doc.doctype, len(classes)))
 
         grids = []
         for item in classes:
             cur_class = str(item["Class"])
             cur_name = str(item["Zone"])
-            utils.msg("Calculating grid for %s..." % cur_name)
+            utils.msg("Calculating grid for {}...".format(cur_name))
             out_con = ""
             # here come the CONs:
-            out_con = runCon(item["Depth_LowerBounds"], item["Depth_UpperBounds"], \
+            out_con = run_con(item["Depth_LowerBounds"], item["Depth_UpperBounds"], \
                     bathy, cur_class)
-            out_con = runCon(item["Slope_LowerBounds"], item["Slope_UpperBounds"], \
+            out_con = run_con(item["Slope_LowerBounds"], item["Slope_UpperBounds"], \
                     slope, out_con, cur_class)
-            out_con = runCon(item["LSB_LowerBounds"], item["LSB_UpperBounds"], \
+            out_con = run_con(item["SSB_LowerBounds"], item["SSB_UpperBounds"], \
                     bpi_broad, out_con, cur_class)
-            out_con = runCon(item["SSB_LowerBounds"], item["SSB_UpperBounds"], \
+            out_con = run_con(item["LSB_LowerBounds"], item["LSB_UpperBounds"], \
                     bpi_fine, out_con, cur_class)
             if type(out_con) == arcpy.sa.Raster:
-                rast = utils.saveRaster(out_con, "con_{}.tif".format(cur_name))
+                rast = utils.save_raster(out_con, "con_{}.tif".format(cur_name))
                 grids.append(rast)
             else:
                 # fall-through: no valid values detected for this class.
                 warn_msg = """\
                         WARNING: no valid locations found for class {}:
                           depth:     ({}本})
-                          slope:     ({}本}) 
-                          broad BPI: ({}本}) 
+                          slope:     ({}本})
+                          broad BPI: ({}本})
                           fine BPI:  ({}本})""".format(\
                                 cur_name, item["Depth_LowerBounds"],
                                 item["Depth_UpperBounds"], item["Slope_LowerBounds"],
-                                item["Slope_UpperBounds"], item["LSB_LowerBounds"],
-                                item["LSB_UpperBounds"], item["SSB_LowerBounds"],
-                                item["SSB_UpperBounds"])
+                                item["Slope_UpperBounds"], item["SSB_LowerBounds"],
+                                item["SSB_UpperBounds"], item["LSB_LowerBounds"],
+                                item["LSB_UpperBounds"])
                 utils.msg(textwrap.dedent(warn_msg))
 
         utils.msg("Creating Benthic Terrain Classification Dataset...")
@@ -101,25 +104,24 @@ def main(classification_file, bpi_broad, bpi_fine, slope, bathy,
         for i in range(1, len(grids)):
             utils.msg("{} of {}".format(i, len(grids)-1))
             merge_grid = Con(merge_grid, grids[i], merge_grid, "VALUE = 0")
-        utils.msg("Saving Output to %s" % out_raster)
-        merge_grid.save(out_raster)
 
         arcpy.env.rasterStatistics = "STATISTICS"
+        utils.msg("Saving Output to {}".format(out_raster))
+        merge_grid.save(out_raster)
         utils.msg("Complete.")
 
         # Delete all intermediate raster data sets
-        arcpy.AddMessage("Deleting intermediate data...")
-        paths = []
+        utils.msg("Deleting intermediate data...")
         for grid in grids:
             arcpy.Delete_management(grid.catalogPath)
- 
+
     except Exception as e:
         if type(e) is ValueError:
             raise e
         utils.msg(e, mtype='error')
 
 # when executing as a standalone script get parameters from sys
-if __name__=='__main__':
+if __name__ == '__main__':
     config.mode = 'script'
     main(
         classification_file=sys.argv[1],
