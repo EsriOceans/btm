24f5ed5a6c53773e84fc4d4e31950e10e00bb31a
Shaun Walbridge <shaun.walbridge@gmail.com>
2014-04-23 14:27:58 -0400

    just show diff scales

--- Install/toolbox/scripts/bpi-scipy.py
+++ Install/toolbox/scripts/bpi-scipy.py
@@ -19,13 +19,12 @@ for i in xrange(25):
     print "running {}".format(size)
     med = nd.median_filter(r, size)
    
-    if i > 0:
-        rm = med - prev
-        a = fig.add_subplot(5, 5,i+1)
-        plt.imshow(rm, interpolation='nearest')
-        a.set_title('bpi {}x{}'.format(size, size))
-        plt.axis('off')
-        plt.subplots_adjust(hspace = 0.1)
+    #rm = med - prev
+    a = fig.add_subplot(5, 5,i+1)
+    plt.imshow(med, interpolation='nearest')
+    a.set_title('{}x{}'.format(size, size))
+    plt.axis('off')
+    plt.subplots_adjust(hspace = 0.1)
     prev = med
 #fig.tight_layout()
 #plt.colorbar()
@@ -34,10 +33,10 @@ for i in xrange(25):
 #rr.save("Z:/data/workspace/fizzbo.tif")
 
 
-plt.savefig("test.png", bbox_inches='tight')
+plt.savefig("btm-scale-compare.png", bbox_inches='tight')
 print "scipy time: %.2f" % (time.time() - t0)
 
 #outRaster = Int(Plus(Times(Divide(
 #                Minus(bpi_raster, float(bpi_mean)), float(bpi_std_dev)), 100), 0.5))
  
-((r - r.mean()) / r.std()) * 100
+# ((r - r.mean()) / r.std()) * 100
