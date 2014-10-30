f1685c9a5b5282bf6dac97198c0c3e5e0e2ae566
Shaun Walbridge <shaun.walbridge@gmail.com>
2014-10-30 17:32:01 -0400

    PEP8

--- Install/btm_addin.py
+++ Install/btm_addin.py
@@ -7,19 +7,23 @@ import pythonaddins
 
 # enable local imports
 local_path = os.path.dirname(__file__)
-
 btm_toolbox = os.path.join(local_path, "toolbox", "btm.pyt")
 
-# ignore exceptions with this class. Our workaround for current limiations
-# in pythonaddins.GPToolDialog.
+
 class DevNull:
+    """
+    Ignore exceptions with this class. Our workaround for current limiations
+    in pythonaddins.GPToolDialog.
+    """
     def write(self, msg):
         pass
+
 
 def tool_dialog(toolbox, tool):
     """Error-handling wrapper around pythonaddins.GPToolDialog."""
     # FIXME: either have to supress stderr here or always get an error.
     # Contacted internal resources to figure out what's going on.
+    # This is NIM089253 / CR245605
     try:
         err_default = sys.stderr
         sys.stderr = DevNull()
@@ -30,21 +34,21 @@ def tool_dialog(toolbox, tool):
             "{toolbox}, {tool}))".format(toolbox=toolbox, tool=tool)
     return None
 
-## Run all steps (wizard)
+
 class ButtonRunBTMSteps(object):
-    """Implementation for runBTMSteps.button (Button)"""
+    """Run all steps (wizard)"""
     def __init__(self):
         self.enabled = True
         self.checked = False
+
     def onClick(self):
         tool_dialog(btm_toolbox, 'runfullmodel')
         # XXX: trying to call multiple tools just kills ArcMap.
         # adding a sleep() just slows down the train wreck. Not yet sure
         # of the best approach to actually get this to work.
-        # Still broken after SP1, so it may take till SP2 before a fix
-        # is available.
-        #tool_dialog(btm_toolbox, 'finescalebpi')
-        #tool_dialog(btm_toolbox, 'standardizebpi')
+        # Still broken at 10.3
+        # tool_dialog(btm_toolbox, 'finescalebpi')
+        # tool_dialog(btm_toolbox, 'standardizebpi')
         # update: this bug is documented in NIM085804; no current plans to fix it.
         #         Only one call is possible from an onClick event.
 
@@ -53,122 +57,145 @@ class RunBTMSteps(object):
     """Implementation for RunBTMSteps.tool (Tool)"""
     def __init__(self):
         self.enabled = True
-        self.shape = "NONE" # Can set to "Line", "Circle" or
-                            # "Rectangle" for interactive shape drawing
+        # Can set to 'Line', 'Circle' or 'Rectangle' for
+        # interactive shape drawing
+        self.shape = "NONE"
+
     def onClick(self):
         tool_dialog(btm_toolbox, 'runfullmodel')
 
-## classification functions
+"""Classification functions"""
+
+
 class classifyBenthicTerrain(object):
     """Implementation for classifyBenthicTerrain.button (Button)"""
     def __init__(self):
         self.enabled = True
         self.checked = False
+
     def onClick(self):
         tool_dialog(btm_toolbox, 'classifyterrain')
 
+#
+# BPI specific functions
 
-## BPI specific functions
+
 class calculateBroadBPI(object):
     """Implementation for caculateBroadBPI.button (Button)"""
     def __init__(self):
         self.enabled = True
         self.checked = False
+
     def onClick(self):
         tool_dialog(btm_toolbox, 'broadscalebpi')
+
 
 class calculateFineBPI(object):
     """Implementation for caculateFineBPI.button (Button)"""
     def __init__(self):
         self.enabled = True
         self.checked = False
+
     def onClick(self):
         tool_dialog(btm_toolbox, 'finescalebpi')
+
 
 class standarizeBPI(object):
     """Implementation for standarizeBPI.button (Button)"""
     def __init__(self):
         self.enabled = True
         self.checked = False
+
     def onClick(self):
         tool_dialog(btm_toolbox, 'standardizebpi')
 
-## Geomorphometry Functions
-
 #
-# Convienience functions pulled in from spatial analyst
-#
+# Geomorphology convienience functions pulled in from spatial analyst
 
 
-# arcpy.sa.Curvature() [can produce plan AND profile curvature through this one tool].
 class calculateCurvature(object):
-    """Implementation for calculateCurvature.button (Button)"""
+    """
+    Implementation of arcpy.sa.Curvature(): can produce plan AND profile
+    curvature through this one tool.
+    """
     def __init__(self):
         self.enabled = True
         self.checked = False
-    def onClick(self):
-        tool_dialog("Spatial Analyst Tools", 'Curvature')
 
-# arcpy.sa.Aspect()
+    def onClick(self):
+        tool_dialog('Curvature_sa')
+
+
 class calculateAspect(object):
-    """Implementation for calculateAspect.button (Button)"""
+    """Wrapper for Aspect() from Spatial Analyst"""
     def __init__(self):
         self.enabled = True
         self.checked = False
-    def onClick(self):
-        tool_dialog("Spatial Analyst Tools", 'Aspect')
 
-# computes the trignometric identities for the circular variable aspect
+    def onClick(self):
+        tool_dialog('Aspect_sa')
+
+
 class calculateStatisticalAspect(object):
-    """Implementation for calculateAspect.button (Button)"""
+    """
+    Computes the trignometric identities for the circular variable aspect.
+    """
     def __init__(self):
         self.enabled = True
         self.checked = False
+
     def onClick(self):
         tool_dialog(btm_toolbox, "statisticalaspect")
 
-#- variance in depth
-#- mean water depth
-#    + [sum(depth) / n cells]
-#    + arcpy.sa.Focal()
-#- std dev. water depth
-#    + stddev = sqrt(variance)
-#    + arcpy.sa.Focal()
+
 class calculateDepthStatistics(object):
-    """Implementation for calculateDepthStatistics.button (Button)"""
+    """
+    Depth statistics computes:
+        - variance in depth
+        - mean water depth
+            + [sum(depth) / n cells]
+            + arcpy.sa.Focal()
+        - std dev. water depth
+            + stddev = sqrt(variance)
+            + arcpy.sa.Focal()
+    """
     def __init__(self):
         self.enabled = True
         self.checked = False
+
     def onClick(self):
         tool_dialog(btm_toolbox, 'depthstatistics')
+        # TODO: handle adding the statistics to the current map when run
 
-        # XXX: handle adding the statistics to the current map when run
-
-
-# New geomorphometry functions
 #
+# New geomorphometry functions
+
 
 class calculateSaPa(object):
     """Implementation for calculateSaPa.button (Button)"""
     def __init__(self):
         self.enabled = True
         self.checked = False
+
     def onClick(self):
         tool_dialog(btm_toolbox, 'surfacetoplanar')
+
 
 class calculateTerrainRuggedness(object):
     """Implementation for calculateTerrainRuggedness.button (Button)"""
     def __init__(self):
         self.enabled = True
         self.checked = False
+
     def onClick(self):
         tool_dialog(btm_toolbox, 'terrainruggedness')
 
-# wrapped with parameters set
+
 class calculateSlope(object):
-    """Implementation for calculateSlope.button (Button)"""
+    """Executes Slope() from Spatial Analyst, with units set to degrees."""
     def __init__(self):
         self.enabled = True
         self.checked = False
+
     def onClick(self):
         tool_dialog(btm_toolbox, 'btmslope')
