35a2db2a7e05bc3b16dd5c913df068776e689f3a
Shaun Walbridge <shaun.walbridge@gmail.com>
2015-03-11 08:40:22 -0700

    Remove context manager
    Using a context manager is causing strange issues such as
    signing failure. Removing.

--- makeaddin.py
+++ makeaddin.py
@@ -11,8 +11,10 @@ if len(sys.argv) == 2:
         VERBOSE_MODE = True
 
 current_path = os.path.dirname(os.path.abspath(__file__))
+
 out_zip_name = os.path.join(current_path, "..",
                             os.path.basename(current_path) + ".esriaddin")
+
 backup_patterns = {
     'PLUGIN_BACKUP_PATTERN': re.compile(r'.*_addin_[0-9]+[.]py$', re.IGNORECASE),
     'VIM_SWAP_PATTERN': re.compile(r'.*\.sw[op]$', re.IGNORECASE),
@@ -21,36 +23,29 @@ backup_patterns = {
     'OSX_DSTORE': re.compile('.DS_Store')
 }
 
-
 def looks_like_a_backup(filename):
-    """Determine if files are ignorable."""
     is_backup = False
-    for pattern in backup_patterns.values():
+    for (name, pattern) in backup_patterns.items():
         if bool(pattern.match(filename)):
             is_backup = True
     return is_backup
 
 # create a new zip file object, set it to be compressed.
-with zipfile.ZipFile(out_zip_name, 'w', zipfile.ZIP_DEFLATED) as zip_file:
-    # specifically select the top-level files for inclusion
-    top_level_files = [
-        'config.xml', 'README.md', 'makeaddin.py',
-        'LICENSE', 'AUTHORS', 'CHANGELOG'
-    ]
-    for top_level_file in top_level_files:
-        zip_file.write(os.path.join(current_path, top_level_file), top_level_file)
-    # add all contents from these directories
-    dirs_to_add = ['Images', 'Install']
-    for directory in dirs_to_add:
-        for (path, dirs, files) in os.walk(os.path.join(current_path, directory)):
-            archive_path = os.path.relpath(path, current_path)
-            found_file = False
-            for fn in (f for f in files if not looks_like_a_backup(f)):
-                archive_file = os.path.join(archive_path, fn)
-                if VERBOSE_MODE:
-                    print archive_file
-                zip_file.write(os.path.join(path, fn), archive_file)
-                found_file = True
-            if not found_file:
-                zip_file.writestr(os.path.join(archive_path, 'placeholder.txt'),
-                                  "(Empty directory)")
+zip_file = zipfile.ZipFile(out_zip_name, 'w', zipfile.ZIP_DEFLATED)
+for filename in ('config.xml', 'README.md', 'makeaddin.py'):
+    zip_file.write(os.path.join(current_path, filename), filename)
+dirs_to_add = ['Images', 'Install']
+for directory in dirs_to_add:
+    for (path, dirs, files) in os.walk(os.path.join(current_path, directory)):
+        archive_path = os.path.relpath(path, current_path)
+        found_file = False
+        for fn in (f for f in files if not looks_like_a_backup(f)):
+            archive_file = os.path.join(archive_path, fn)
+            if VERBOSE_MODE:
+                print archive_file
+            zip_file.write(os.path.join(path, fn), archive_file)
+            found_file = True
+        if not found_file:
+            zip_file.writestr(os.path.join(archive_path, 'placeholder.txt'),
+                              "(Empty directory)")
+zip_file.close()
